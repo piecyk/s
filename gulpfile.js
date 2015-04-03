9ec3f9e3b7c60ab492999ca6207f1e4f1d63c9fa
piecyk <piecyk@gmail.com>
2015-04-04 00:39:41 +0100

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,6 @@ var config = require('./webpack.config.js');
 
 function onBuild(done) {
   return function(err, stats) {
-    if(err) throw new gutil.PluginError("webpack", err);
     gutil.log("[webpack]", stats.toString());
     done();
   };
@@ -21,11 +20,11 @@ gulp.task('backend-build', function(done) {
 gulp.task('backend-watch', function(done) {
   var isDone = false;
   webpack(config).watch(100, function(err, stats) {
-    if (err) throw new gutil.PluginError("[webpack]", err);
     if (!isDone) {
       isDone = true;
       done();
     }
+    gutil.log("[webpack]", stats.toString());
     nodemon.restart();
   });
 });
